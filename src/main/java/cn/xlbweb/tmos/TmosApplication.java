d3bed8c809fbc03e247af27cf2886a778e67ba1f
bobi1234 <jxjy.ing@foxmail.com>
2018-07-16 10:23:12 +0800

    创建项目基本目录

---
+++ src/main/java/cn/xlbweb/tmos/TmosApplication.java
@@ -0,0 +1,12 @@
+package cn.xlbweb.tmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmosApplication.class, args);
+    }
+}

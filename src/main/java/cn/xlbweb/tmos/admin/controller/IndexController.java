a04f1d3cca67f3ad06e5cc6250b1a442dcc0ea00
bobi1234 <jxjy.ing@foxmail.com>
2018-07-17 10:13:55 +0800

    加入后台CMS模板

---
+++ src/main/java/cn/xlbweb/tmos/admin/controller/IndexController.java
@@ -0,0 +1,28 @@
+package cn.xlbweb.tmos.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author: bobi
+ * @date: 2018/7/17 09:45
+ * @description:
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping("admin")
+    public String index() {
+        return "admin/index";
+    }
+
+    @GetMapping("admin/welcome")
+    public String welcome() {
+        return "admin/welcome";
+    }
+
+    @GetMapping("admin/login")
+    public String login() {
+        return "admin/login";
+    }
+}
